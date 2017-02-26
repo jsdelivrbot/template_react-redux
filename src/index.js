b1c25d53be150ca07b01731f6b4ae9df86f873fb
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2017-02-25 19:53:49 -0800

    added react router and added surgehack

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {Router, browserHistory} from 'react-router';
+import Routes from './routes';
 import ReduxPromise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
@@ -12,6 +13,6 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)} >
-        <App />
+        <Router routes={Routes} history={browserHistory} />
     </Provider>
     , document.querySelector('.container'));
